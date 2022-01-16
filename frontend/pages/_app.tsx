e85cb1efbadff9949c6e75b2d55ed4f75ff0e6a0
Michel Dore <michel@doremail.nl>
2022-01-16 20:11:24 +0100

    Responsive

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,31 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+          <Head>
+            <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-XWBGLHF4DQ"
+            />
+
+            <script
+            dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-XWBGLHF4DQ', { page_path: window.location.pathname });
+                `,
+            }}
+            />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+          </Head>
+          <Component {...pageProps} />
+        </>
+      )
 }
 
 export default App
