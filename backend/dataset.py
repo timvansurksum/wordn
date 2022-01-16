441057a04a5f03d0a8ae7e4db372d058ad90a6ea
Tim van Surksum <325605@student.mboutrecht.nl>
2022-01-16 15:32:56 +0100

    random word generator done

--- backend/dataset.py
+++ backend/dataset.py
@@ -1,11 +1,22 @@
+import codecs
+import os
+import random
+
+
 
 class Dataset:
 
     def getRandomWord(self, category = "standard", length = 5):
-        return "Tim"
+        datasets = os.listdir('datasets')
+        for dataset in datasets:
+            if (dataset.find(category) + 1):
+                path = os.path.abspath(f"datasets/{dataset}")
+                data = open(path, 'r')
+                data = data.read().split('\n')
+                return random.choice(data)
     
-    def getCategories():
+    def getCategories(self):
         return []
     
-
-
+dataset = Dataset()
+print(dataset.getRandomWord())
