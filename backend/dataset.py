332b5f966d5f70992b8cdcfa7271e8d8b2b27d73
Tim van Surksum <325605@student.mboutrecht.nl>
2022-01-16 16:43:45 +0100

    created dataset splitter by length

--- backend/dataset.py
+++ backend/dataset.py
@@ -6,17 +6,39 @@ import random
 
 class Dataset:
 
-    def getRandomWord(self, category = "standard", length = 5):
-        datasets = os.listdir('datasets')
-        for dataset in datasets:
-            if (dataset.find(category) + 1):
-                path = os.path.abspath(f"datasets/{dataset}")
-                data = open(path, 'r')
-                data = data.read().split('\n')
-                return random.choice(data)
+    def split_dataset(self, category):
+        path = os.path.abspath(f"datasets/{category}.txt")
+        words = open(path, 'r')
+        linesep = os.linesep
+        words = words.read().split('\n')
+        
+        word_length_dictionairy = {}
+        for word in words:
+            word_length = len(word)
+            if word_length in word_length_dictionairy.keys():
+                word_length_dictionairy[word_length].append(word)
+            else:
+                word_length_dictionairy[len(word)] = []
+        for word_length_key in word_length_dictionairy.keys():
+            folder_path = os.path.abspath("datasets")
+            words_length_file = open(f"{folder_path}/{category}_{word_length_key}.txt", 'w')
+            words = ''
+            for word in word_length_dictionairy[word_length_key]:
+                words_length_file.write(f'{word}\n')
+
+
+
+    def getRandomWord(self, requested_category = "standard", length = 5):
+        if (length > 3) and length < 9:
+            categories = os.listdir('datasets')
+            for category in categories:
+                if (category.find(requested_category) + 1):
+                    path = os.path.abspath(f"datasets/{requested_category}_{length}.txt")
+                    data = open(path, 'r')
+                    data = data.read().split('\n')
+                    return random.choice(data).lower()
+        else:
+            return 'error wrong word length'
     
     def getCategories(self):
         return []
-    
-dataset = Dataset()
-print(dataset.getRandomWord())
