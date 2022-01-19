4e9c1fbf48499fb155092211f7753fd8172a8d5d
Vince van Noort <vincevannoort@gmail.com>
2022-01-20 00:36:11 +0100

    Make local development easier, and fix finished game bug

--- backend/game.py
+++ backend/game.py
@@ -1,8 +1,16 @@
+import os
+from dotenv import load_dotenv
 from dataset import Dataset
 from tokenGenerator import Token
+load_dotenv()
+show_word_in_logs = os.getenv('SHOW_WORD_IN_LOGS', False)
 
 class Game:
     def start(self, length: int, category: str):
         dataset = Dataset()
         word = dataset.getRandomWord(category, length)
+
+        if (show_word_in_logs == 'true'):
+            print(f'new word for user is: {word}')
+            
         return Token().saveWordInTokenAndGetToken(word)
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-import { createRef, useCallback, useEffect, useRef, useState } from 'react';
+import { createRef, SetStateAction, useCallback, useEffect, useRef, useState } from 'react';
 import styles from '../styles/Home.module.css';
 import axios from 'axios';
 import { findSourceMap } from 'module';
@@ -17,12 +17,12 @@ type Letter = {
   state: LetterState
 }
 
-// const URL = 'http://localhost:4000'
+console.log(`node env: ${process.env.NODE_ENV}`)
 const env = process.env.NODE_ENV
 const URL = (env == "development")
   ? 'http://localhost:4000'
   : 'https://backend.wordn.nl'
-// : 'https://wordn-backend-55tqlstrca-ez.a.run.app'
+
 
 function LetterBlock({ letter }: { letter: Letter }): JSX.Element {
   return (
@@ -46,91 +46,73 @@ function WordRow({ word, incorrectWord }: { word: Letter[], incorrectWord: boole
 }
 
 const Application: NextPage = () => {
-  /** variables */
+  /**
+   * APPLICATION STATE
+   */
   const [incorrectWord, setIncorrectWord] = useState<boolean>(false);
+  const [finished, setFinished] = useState<boolean>(false);
   const [token, setToken] = useState<string>('');
   const [validations, setValidation] = useState([]);
   const [tries, setTries] = useState(0);
   const [oldWords, setOldWords] = useState<Array<Letter[]>>([]);
-  const [word, setWord] = useState<Letter[]>([
-    getEmptyLetter()
-  ]);
+  const [word, setWord] = useState<Letter[]>([]);
 
-  function getEmptyLetter(): Letter {
-    return {
-      char: '',
-      state: LetterState.Empty
-    }
+  function resetGame() {
+    setIncorrectWord(false)
+    setFinished(false)
+    setTries(0)
+    setOldWords([])
+    startNewGame(setToken, setWord);
   }
 
-  function getEmptyWord(): Letter[] {
-    return [
-      getEmptyLetter(),
-      getEmptyLetter(),
-      getEmptyLetter(),
-      getEmptyLetter(),
-      getEmptyLetter(),
-    ]
-  }
-
-  /** set token */
+  /**
+   * START GAME ON LOAD
+   */
   useEffect(() => {
-    console.log('set token')
-
-    /** start for user */
-    axios.get(`${URL}/start?length=5&category=standard`).then((response) => {
-      setToken(response.data)
-    })
-
-    /** set standard word length */
-    setWord(getEmptyWord())
+    startNewGame(setToken, setWord);
   }, [])
 
-  function translateServerStateToFrontendState(serverState: string): LetterState {
-    if (serverState == 'WrongPosition') {
-      return LetterState.IncorrectPosition
-    }
-    if (serverState == 'Wrong') {
-      return LetterState.Incorrect
-    }
-    if (serverState == 'Correct') {
-      return LetterState.Correct
-    }
-    throw Error('could not find state')
-  }
-
-  /** check validations */
+  /** 
+   * UPDATE TO VALIDATIONS
+   * check whether we have to update word state
+   **/
   useEffect(() => {
-    console.log('etest')
-    // console.log(Object.entries(validations))
-    // console.log(Object.entries(validations).length)
-    if (Object.entries(validations).length == 0) {
-      if (word[0].state == LetterState.Empty) return
+
+    // check if user is finished
+    if (word.length == 0) return
+
+    // check if user has filled anything
+    const startState = word[0].state == LetterState.Empty
+    if (startState) return
+
+    // check if server returned that we have a invalid word
+    const invalidWord = Object.entries(validations).length == 0
+    if (invalidWord) {
       setWord(getEmptyWord())
       setIncorrectWord(true)
       return
     }
 
+    // update word with validation
     const newWord: Letter[] = []
-    Object.entries(validations).forEach(([key, value], _) => {
-      newWord.push({
-        char: key[0],
-        state: translateServerStateToFrontendState(value),
-      })
-    })
+    updateWordWithValidations(validations, newWord);
+
+    // give the user a new try
     const newOldWords = [...oldWords, newWord]
-    const wordCorrect = Object.entries(validations).every(([key, value]) => translateServerStateToFrontendState(value) == LetterState.Correct)
     setOldWords(newOldWords)
     setIncorrectWord(false)
+
+    // check if word is correct
+    const wordCorrect = isWordCorrect(validations)
     if (wordCorrect) {
       setWord([])
+      setFinished(true)
     } else {
       /** new try */
       setWord(getEmptyWord())
     }
 
     setTimeout(() => {
-      // window.scrollTo(0, document.body.scrollHeight);
       window.scrollTo({
         top: document.body.scrollHeight,
         behavior: 'smooth'
@@ -139,7 +121,9 @@ const Application: NextPage = () => {
   }, [validations])
 
 
-  /** handle all key presses */
+  /** 
+   * ADD KEY HANDLERS
+   */
   useEffect(() => {
     const alphabet = new Set(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'r', 'z'])
 
@@ -159,41 +143,43 @@ const Application: NextPage = () => {
       setWord(wordToAdjust);
     }
 
-    function getStringFromWord() {
+    function getStringFromWord(): string {
       return word.map((letter) => letter.char).join('')
     }
 
-    async function validateWord() {
+    async function validateWord(): Promise<void> {
       const emptyLetter = word.find(letter => letter.state == LetterState.Empty)
-      /** cannot send word  */
-      if (emptyLetter) {
-        console.log('iets')
+
+      // check if user is finished
+      if (finished) {
+        resetGame()
+        return
       }
 
-      /** can send word */
-      else {
-        try {
-          /** say word */
-          const chars = word.map((letter) => letter.char).join('')
-          const charWithDot = word.map((letter) => letter.char).map(char => `${char}. `).join('')
-          let utterance = new SpeechSynthesisUtterance(`${chars}! ${charWithDot}`);
-          speechSynthesis.speak(utterance);
+      /** cannot send word  */
+      if (emptyLetter) return
 
-          const data = {
-            token: token,
-            word: getStringFromWord()
-          }
-          const response = await axios.post(`${URL}/validate_word`, data)
-          setToken(response.data.token)
-          setValidation(response.data.validation)
-          setTries(response.data.tries)
-        } catch (error) {
-          console.log(error)
+      try {
+        /** say word */
+        const chars = word.map((letter) => letter.char).join('')
+        const charWithDot = word.map((letter) => letter.char).map(char => `${char}. `).join('')
+        let utterance = new SpeechSynthesisUtterance(`${chars}! ${charWithDot}`);
+        speechSynthesis.speak(utterance);
+
+        const data = {
+          token: token,
+          word: getStringFromWord()
         }
+        const response = await axios.post(`${URL}/validate_word`, data)
+        setToken(response.data.token)
+        setValidation(response.data.validation)
+        setTries(response.data.tries)
+      } catch (error) {
+        console.log(error)
       }
     }
 
-    function removeLastLetter() {
+    function removeLastLetter(): void {
       /** find the first filled letter at the end */
       let wordToReverse = [...word]
       const filledLetterReverseIndex = wordToReverse.reverse().findIndex(letter => letter.state == LetterState.Filled)
@@ -225,7 +211,7 @@ const Application: NextPage = () => {
     return () => {
       window.removeEventListener('keydown', handleEvent)
     }
-  }, [token, word]);
+  }, [token, word, finished]);
 
   const textInput = useRef(null);
 
@@ -234,14 +220,6 @@ const Application: NextPage = () => {
     // @ts-ignore
     textInput.current.focus()
   }
-
-  // useEffect(() => {
-  //     // setTimeout(() => {
-  //     //     focusTextInput()
-  //     // }, 250)
-  //     document.addEventListener('click', focusTextInput)
-  //     return document.removeEventListener('click', focusTextInput)
-  // }, [])
 
   return (
     <div className={styles['container']}>
@@ -260,6 +238,58 @@ const Application: NextPage = () => {
       </main>
     </div>
   )
+}
+
+function startNewGame(setToken: { (value: SetStateAction<string>): void; (value: SetStateAction<string>): void; (arg0: any): void; }, setWord: { (value: SetStateAction<Letter[]>): void; (value: SetStateAction<Letter[]>): void; (arg0: Letter[]): void; }) {
+  axios.get(`${URL}/start?length=5&category=standard`).then((response) => {
+    setToken(response.data);
+  });
+
+  /** set standard word length */
+  setWord(getEmptyWord());
+}
+
+function getEmptyLetter(): Letter {
+  return {
+    char: '',
+    state: LetterState.Empty
+  }
+}
+
+function getEmptyWord(): Letter[] {
+  return [
+    getEmptyLetter(),
+    getEmptyLetter(),
+    getEmptyLetter(),
+    getEmptyLetter(),
+    getEmptyLetter(),
+  ]
+}
+
+function translateServerStateToFrontendState(serverState: string): LetterState {
+  if (serverState == 'WrongPosition') {
+    return LetterState.IncorrectPosition
+  }
+  if (serverState == 'Wrong') {
+    return LetterState.Incorrect
+  }
+  if (serverState == 'Correct') {
+    return LetterState.Correct
+  }
+  throw Error('could not find state')
+}
+
+function isWordCorrect(validations: never[]) {
+  return Object.entries(validations).every(([key, value]) => translateServerStateToFrontendState(value) == LetterState.Correct);
+}
+
+function updateWordWithValidations(validations: never[], newWord: Letter[]) {
+  Object.entries(validations).forEach(([key, value], _) => {
+    newWord.push({
+      char: key[0],
+      state: translateServerStateToFrontendState(value),
+    });
+  });
 }
 
 export default Application
