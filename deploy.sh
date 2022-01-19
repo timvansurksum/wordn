29b2e80a38a943e59224b49a13d3df8db3970380
Vince van Noort <vincevannoort@gmail.com>
2022-01-20 00:35:29 +0100

    make local development easier, and fix finished game bug

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-docker-compose build
-docker-compose push
-gcloud run deploy wordn-frontend --project=wordn-338419 --port=3000 --max-instances=100 --allow-unauthenticated --region=europe-west4 --image=europe-west4-docker.pkg.dev/wordn-338419/images/wordn.frontend:latest
-gcloud run deploy wordn-backend --project=wordn-338419 --port=4000 --max-instances=100 --allow-unauthenticated --region=europe-west4 --image=europe-west4-docker.pkg.dev/wordn-338419/images/wordn.backend:latest
