6460e9978120f97eaf945d0d655ed46ed1faa349
Michel Dore <michel@doremail.nl>
2022-01-19 22:52:26 +0100

    Added favicon and SEO tags

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -5,27 +5,38 @@ import Head from 'next/head'
 function App({ Component, pageProps }: AppProps) {
     return (
         <>
-          <Head>
-            <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-XWBGLHF4DQ"
-            />
+            <Head>
+                <script
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=G-XWBGLHF4DQ"
+                />
+                <title>Woordl | Nederlandse versie van Wordle</title>
 
-            <script
-            dangerouslySetInnerHTML={{
-                __html: `
+
+                <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png"></link>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+                <link rel="manifest" href="/site.webmanifest"></link>
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"></link>
+                <meta name="msapplication-TileColor" content="#da532c"></meta>
+                <meta name="theme-color" content="#ffffff"></meta>
+                <meta name="description" content="Zes beurten om het woord te gokken. Elke dag een nieuwe puzzel."></meta>
+                
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
                 gtag('config', 'G-XWBGLHF4DQ', { page_path: window.location.pathname });
                 `,
-            }}
-            />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
-          </Head>
-          <Component {...pageProps} />
+                    }}
+                />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+            </Head>
+            <Component {...pageProps} />
         </>
-      )
+    )
 }
 
 export default App
