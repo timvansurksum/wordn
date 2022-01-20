6979232b7594b2bc40a92c36e730028c16c96249
Vince van Noort <vincevannoort@gmail.com>
2022-01-20 19:42:33 +0100

    Revert "renewed word list & updated split dataset function"
    This reverts commit bf7524fc2423b72b41f6be39ed30cf1067bda6e6.

--- backend/dataset.py
+++ backend/dataset.py
@@ -1,13 +1,13 @@
 import os
 import random
-import string
 
 
 class Dataset:
+
     def split_dataset(self, category):
         path = os.path.abspath(f"datasets/{category}.txt")
-        words = open(path, "r")
-        words = words.read().split("\n")
+        words = open(path, 'r')
+        words = words.read().split('\n')
 
         word_length_dictionairy = {}
         for word in words:
@@ -20,35 +20,31 @@ class Dataset:
         for word_length_key in word_length_dictionairy.keys():
             folder_path = os.path.abspath("datasets")
             words_length_file = open(
-                f"{folder_path}/length_sets/{category}_{word_length_key}.txt", "w"
-            )
-            words = ""
+                f"{folder_path}/length_sets/{category}_{word_length_key}.txt", 'w')
+            words = ''
             for word in word_length_dictionairy[word_length_key]:
-                word = word.lower()
-                char_check = all([char in string.ascii_lowercase for char in word])
-                if char_check:
-                    words_length_file.write(f"{word}\n")
+                if not (word.find(' ')+1):
+                    words_length_file.write(f'{word}\n')
 
     def getRandomWord(self, requested_category="standard", length=5):
         if (length > 3) and length < 9:
-            categories = os.listdir("datasets")
+            categories = os.listdir('datasets')
             for category in categories:
-                if category.find(requested_category) + 1:
+                if (category.find(requested_category) + 1):
                     path = os.path.abspath(
-                        f"datasets/length_sets/{requested_category}_{length}.txt"
-                    )
-                    data = open(path, "r")
-                    data = data.read().split("\n")
+                        f"datasets/length_sets/{requested_category}_{length}.txt")
+                    data = open(path, 'r')
+                    data = data.read().split('\n')
                     return random.choice(data).lower()
         else:
-            return "error wrong word length"
+            return 'error wrong word length'
 
     def getCategories(self):
-        categories = os.listdir("datasets")
+        categories = os.listdir('datasets')
         clean_categories = []
         for category in categories:
-            if category.endswith(".txt"):
-                clean_categories.append(category.split(".")[0])
+            if category.endswith('.txt'):
+                clean_categories.append(category.split('.')[0])
         return clean_categories
 
     def checkWord(self, word: str):
@@ -57,17 +53,18 @@ class Dataset:
             categories = self.getCategories()
 
             for category in categories:
-                path = os.path.abspath(f"datasets/{category}.txt")
-                data = open(path, "r")
-                data = data.read().split("\n")
+                path = os.path.abspath(
+                    f"datasets/{category}.txt")
+                data = open(path, 'r')
+                data = data.read().split('\n')
                 for word_check in data:
                     if word_check.lower().startswith(word.lower()):
                         return True
             return False
         return False
+                    
 
 
 dataset = Dataset()
-dataset.split_dataset("standard")
-# word = 'verft'
-# print(word, dataset.checkWord(word))
+word = 'verft'
+print(word, dataset.checkWord(word))
